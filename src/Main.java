f9b04904562728b9dacec169b525fc5c5979307c
ssofaaa07 <s.butovetskaya@gmail.com>
2023-03-14 20:44:46 +0300

    create console interface

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,76 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    public static void solution(int sizeOfAlphabet, String words, String text) {
+        Trie trie = createTrie(sizeOfAlphabet, words);
+
+        AlgorithmOfAhoCorasick alg = new AlgorithmOfAhoCorasick(trie);
+
+        Vertex v = trie.root;
+
+        System.out.println("Позиции конца найденных подстрок в тексте:");
+
+        for (int i = 0; i < text.length(); i++) {
+            v = alg.go(v, text.charAt(i));
+            if (v.isTerminal) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static Trie createTrie(int sizeOfAlphabet, String words){
+        ArrayList<String> listOfWords = new ArrayList<>();
+
+        System.out.println("Полученные слова:");
+        int k = 0;
+        for (int i = 0; i < words.length() - 1; i++) {
+            if (words.charAt(i) == ' ') {
+                System.out.println(words.substring(k, i));
+                listOfWords.add(words.substring(k, i));
+                k = i + 1;
+            }
+        }
+        System.out.println(words.substring(k));
+        System.out.println();
+        listOfWords.add(words.substring(k));
+
+        Trie trie = new Trie(sizeOfAlphabet);
+
+        for (String s : listOfWords) {
+            trie.add(s);
+        }
+
+        return trie;
+    }
+
+    public static void start(){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите через пробел несколько строк для дальнейшего поиска:");
+
+        String words = scan.nextLine();
+
+        System.out.println("Введите размер алфавита:");
+
+        int sizeOfAlphabet = scan.nextInt();
+
+        System.out.println("Введите текст:");
+
+        String text = scan.next();
+
+        System.out.println();
+
+        solution(sizeOfAlphabet, words, text);
+    }
     public static void main(String[] args) {
-         List list = Arrays.asList("he", "she", "his", "hers");
+        start();
+    }
+
+    public static void mainExample(String[] args) {
+        List list = Arrays.asList("he", "she", "his", "hers");
         int sizeOfAlphabet = 26;
 
         Trie trie = new Trie(sizeOfAlphabet);
@@ -19,16 +84,15 @@ public class Main {
         }
         System.out.println(trie.find("hi"));
 
-        AlgoritmOfAhoCorasick alg = new AlgoritmOfAhoCorasick(trie);
+        AlgorithmOfAhoCorasick alg = new AlgorithmOfAhoCorasick(trie);
 
-        String text = "odfhdosheheghiswphegkdfkjshfks";
+        String text = "doherscoenkhedo";
         Vertex v = trie.root;
 
         for (int i = 0; i < text.length(); i++) {
             v = alg.go(v, text.charAt(i));
             if (v.isTerminal) {
                 System.out.println(i);
-                System.out.println(text.substring(i-3, i+1));
             }
         }
     }
